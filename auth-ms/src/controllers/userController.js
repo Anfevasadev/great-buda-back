7b7c20a55e3b70fe68f51e8fdee873035d64f534
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-11-30 03:38:50 +0000

    Agregar ruta para obtener usuario por ID y manejar errores en el controlador

--- auth-ms/src/controllers/userController.js
+++ auth-ms/src/controllers/userController.js
@@ -10,6 +10,20 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByPk(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener usuario', error });
+  }
+};
 
 export const createUser = async (req, res) => {
   const { username, name, age, email, password, role } = req.body;
--- auth-ms/src/routes/userRoutes.js
+++ auth-ms/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, getAllUsers } from '../controllers/userController.js';
+import { createUser, getAllUsers, getUserById } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/users', getAllUsers);
 router.post('/users/create', createUser);
+router.get('/users/:id', getUserById);
 
 export default router;
