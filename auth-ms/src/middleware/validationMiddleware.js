407cede3ef19fa10d9b5b126f5738582ce9c8efc
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-12-02 06:38:05 +0000

    Actualizar validación del nombre en el middleware para permitir caracteres acentuados y la letra ñ

--- auth-ms/src/middleware/validationMiddleware.js
+++ auth-ms/src/middleware/validationMiddleware.js
@@ -4,7 +4,7 @@ export const validateUser = [
   body('username').isString().notEmpty().withMessage('El nombre de usuario es obligatorio'),
   body('name')
     .isString().notEmpty().withMessage('El nombre es obligatorio')
-    .matches(/^[a-zA-Z\s]+$/).withMessage('El nombre solo puede contener letras y espacios'),
+    .matches(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/).withMessage('El nombre solo puede contener letras y espacios'),
   body('age').isInt({ min: 18 }).withMessage('La edad debe ser un número entero mayor o igual a 18'),
   body('email').isEmail().withMessage('El correo electrónico no es válido'),
   body('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres'),
