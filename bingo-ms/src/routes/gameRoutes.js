93e45bb10706646cec9f4ccd9335ad008eec7685
Andrés Felipe Valencia Sánchez <47144203+Anfevasadev@users.noreply.github.com>
2024-12-02 08:18:59 +0000

    Agregar middleware de autenticación con verificación de token JWT y proteger la ruta de juego

--- bingo-ms/src/routes/gameRoutes.js
+++ bingo-ms/src/routes/gameRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createOrGetActiveGame, deleteAllGames } from '../controllers/gameController.js';
+import { verifyToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/play', createOrGetActiveGame);
+router.post('/play', verifyToken, createOrGetActiveGame);
 router.delete('/delete-all', deleteAllGames);
 
 export default router;
